5ad0c1820181d535b32327b5999ffc5d61dcbdb1
Jaša <78748619+jasasonc@users.noreply.github.com>
2026-06-16 08:05:59 +0200

    Migrate docs to the unified SDyPy docs standard (pydata theme, reST README, conformance)

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -42,10 +42,7 @@ release = '0.29.1'
 extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.intersphinx',
-    'sphinx.ext.ifconfig',
     'sphinx.ext.viewcode',
-    'sphinx.ext.githubpages',
-    # 'rinoh.frontend.sphinx',
     'sphinx_copybutton',
 ]
 
@@ -82,8 +79,7 @@ pygments_style = None
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-# html_theme = 'sphinx_rtd_theme'
-html_theme = 'sphinx_book_theme'
+html_theme = 'pydata_sphinx_theme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
